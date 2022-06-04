c9aaa0c723206ea39ddf343b09507e692167e4a1
Popularkiya <kingawladzinsk@gmail.com>
2022-06-04 15:07:36 +0200

    fixed, allegedly works, im gonna watch a movie now

--- client/ClientNetworkController.py
+++ client/ClientNetworkController.py
@@ -5,7 +5,7 @@ from styles.styles import *
 import tqdm
 import os
 
-SERVER = "192.168.178.25"
+SERVER = "172.18.176.1" # "192.168.178.25"
 PORT = 9090
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 # send 4096 bytes each time step
@@ -48,58 +48,46 @@ class ClientNetworkController:
         while self.running:
             try:
                 message = self.s.recv(BUFFER_SIZE).decode()
-                if SEPARATOR in message: 
-                    # receive the file infos
-                    # receive using client socket, not server socket
-                    filename, filesize = message.split(SEPARATOR)
-                    # remove absolute path if there is
+                if SEPARATOR in message:
+                    filename, filesize, user = message.split(SEPARATOR)
                     filename = os.path.basename(filename)
-                    # convert to integer
                     filesize = int(filesize)
-                    # start receiving the file from the socket
-                    # and writing to the file stream
                     progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
-                    if not os.path.exists(self.clientName):
-                        os.makedirs(self.clientName)
-                    with open(self.clientName + "\\" + filename, "wb") as f:
+                    uploadDir = "downloads\\" + self.clientName
+                    if not os.path.exists(uploadDir):
+                        os.makedirs(uploadDir)
+                    with open(uploadDir + "\\" + filename, "wb") as f:
                         while True:
-                            # read bytes from the socket (receive)
                             bytes_read = self.s.recv(BUFFER_SIZE)
-                            if not bytes_read:    
-                                # nothing is received
-                                # file transmitting is done
-                                f.close()
-                                break
-                            # write to the file the bytes we just received
                             f.write(bytes_read)
-                            # update the progress bar
                             progress.update(len(bytes_read))
-
+                            filesize = filesize - len(bytes_read)
+                            if filesize <= 0:
+                                break
+                    message = user + ": Transferred " + filename
                 self.view.displayMessage(message)
-                # print(message)
             except:
-                # an error will be printed on the command line or console if there's an error
-                # print("An error occured!")
-                # self.s.close()
-                # self.stopCommunication()
+                print("An error occured!")
+                self.s.close()
+                self.stopCommunication()
                 break
 
     def sendMessage(self, msg):
         if self.view is None:
             print("Chat view isn't assigned!")
             return
-        
+
         message = f"{self.clientName}: {msg}"
         self.s.send(message.encode())
-    
+
     def sendFile(self, path):
         if self.view is None:
             print("Chat view isn't assigned!")
             return
-        
+
         filesize = os.path.getsize(path)
         # send the filename and filesize
-        self.s.send(f"{path}{SEPARATOR}{filesize}".encode())
+        self.s.send(f"{path}{SEPARATOR}{filesize}{SEPARATOR}{self.clientName}".encode())
         # start sending the file
         progress = tqdm.tqdm(range(filesize), f"Sending {path}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
         with open(path, "rb") as f:
@@ -112,6 +100,6 @@ class ClientNetworkController:
                 self.s.send(bytes_read)
                 # update the progress bar
                 progress.update(len(bytes_read))
-    
+
     def addView(self, view):
         self.view = view
