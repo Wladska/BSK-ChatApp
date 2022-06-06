b6987119cac78e987dd494bf3af30b77f9f82866
Kinga Władzińska <58950222+Popularkiya@users.noreply.github.com>
2022-06-06 22:44:28 +0200

    use them frames for godssake, amd bugfix in the meantime (#7)

--- client/ClientNetworkController.py
+++ client/ClientNetworkController.py
@@ -4,12 +4,11 @@ from styles.styles import *
 from tkinter import ttk
 import tqdm
 import os
+from frame.Frame import *
+import struct
 
 SERVER = "172.18.176.1" # "192.168.178.25"
 PORT = 9090
-SEPARATOR = "<SEPARATOR>"
-ACKNOWLEDGEMENT = "<ACKNOWLEDGEMENT>"
-BUFFER_SIZE = 1024 # send 1024 bytes each time step
 
 
 class ClientNetworkController:
@@ -33,7 +32,7 @@ class ClientNetworkController:
 
     def stopCommunication(self):
         # send close connection command to server (for that client)
-        self.s.send("!CLOSE".encode())
+        self.sendFrame(Frame(FrameType.CLOSE, ""))
 
         # close connection
         self.running = False
@@ -46,32 +45,32 @@ class ClientNetworkController:
             print("Chat view isn't assigned!")
             return
 
+        default_size = len(Frame(FrameType.SIZE, struct.pack('I', 420)).serialize())
+        size = default_size
+
         while self.running:
             try:
-                message = self.s.recv(BUFFER_SIZE).decode()
-                if SEPARATOR in message:
-                    filename, filesize, user = message.split(SEPARATOR)
-                    filename = os.path.basename(filename)
-                    filesize = int(filesize)
-                    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
+                message = self.s.recv(size)
+                recvFrame = pickle.loads(message)
+                size = default_size
+
+                if recvFrame.type == FrameType.SIZE:
+                    size = struct.unpack('I', recvFrame.data)
+                    size = size[0]
+                elif recvFrame.type == FrameType.ACK:
+                    self.view.displayAcknowledgement(recvFrame.user)
+                elif recvFrame.type == FrameType.FILE_END:
+                    self.view.displayMessage(f"{recvFrame.user}:Sent {recvFrame.fileName}")
+                    self.sendAcknowledgement()
+                elif recvFrame.type == FrameType.MSG:
+                    self.view.displayMessage(f"{recvFrame.user}:{recvFrame.data}")
+                    self.sendAcknowledgement()
+                elif recvFrame.type == FrameType.FILE:
                     uploadDir = "downloads\\" + self.clientName
                     if not os.path.exists(uploadDir):
                         os.makedirs(uploadDir)
-                    with open(uploadDir + "\\" + filename, "wb") as f:
-                        while True:
-                            bytes_read = self.s.recv(BUFFER_SIZE)
-                            f.write(bytes_read)
-                            filesize = filesize - len(bytes_read)
-                            progress.update(len(bytes_read))
-                            if filesize <= 0:
-                                break
-                    message = user + ": Transferred " + filename
-                if ACKNOWLEDGEMENT not in message:
-                    self.view.displayMessage(message)
-                    self.sendAcknowledgement()
-                else:
-                    _, user = message.split(ACKNOWLEDGEMENT)
-                    self.view.displayAcknowledgement(user)
+                    with open(uploadDir + "\\" + recvFrame.fileName, "a+b") as f:
+                        f.write(recvFrame.data)
             except:
                 print("An error occured!")
                 self.s.close()
@@ -83,25 +82,21 @@ class ClientNetworkController:
             print("Chat view isn't assigned!")
             return
 
-        message = f"{self.clientName}: {msg}"
-        self.s.send(message.encode())
+        self.sendFrame(Frame(FrameType.MSG, msg, self.clientName))
 
     def sendAcknowledgement(self):
         if self.view is None:
             print("Chat view isn't assigned!")
             return
 
-        message = f"{ACKNOWLEDGEMENT}{self.clientName}"
-        self.s.send(message.encode())
+        self.sendFrame(Frame(FrameType.ACK, self.clientName, self.clientName))
 
     def sendFile(self, path):
         if self.view is None:
             print("Chat view isn't assigned!")
             return
-
         filesize = os.path.getsize(path)
-        # send the filename and filesize
-        self.s.send(f"{path}{SEPARATOR}{filesize}{SEPARATOR}{self.clientName}".encode())
+        self.sendFrame(Frame(FrameType.FILE, b"", self.clientName, os.path.basename(path)))
         # start sending the file
         progress = tqdm.tqdm(range(filesize), f"Sending {os.path.basename(path)}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
         progressBarPopup = Toplevel()
@@ -117,11 +112,19 @@ class ClientNetworkController:
                 bytes_read = f.read(BUFFER_SIZE)
                 if not bytes_read:
                     break
-                self.s.send(bytes_read)
+
+                self.sendFrame(Frame(FrameType.FILE, bytes_read, self.clientName, os.path.basename(path)))
                 progress.update(len(bytes_read))
                 progressBarPopup.update()
                 barProgress += BUFFER_SIZE
                 progress_var.set(barProgress)
+        self.sendFrame(Frame(FrameType.FILE_END, b"", self.clientName, os.path.basename(path)))
+
+    def sendFrame(self, frame):
+        serialized = frame.serialize()
+        sizeFrame = Frame(FrameType.SIZE, struct.pack('I', len(serialized)))
+        self.s.sendall(sizeFrame.serialize())
+        self.s.sendall(serialized)
 
     def addView(self, view):
         self.view = view
